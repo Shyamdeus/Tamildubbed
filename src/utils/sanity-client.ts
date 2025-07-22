047418e32bd9a6c4f034bc197e7d2e64d83ee70e
Suthagar S <53191065+Shyamdeus@users.noreply.github.com>
2025-07-22 20:39:41 +0530

    Fix missing Sanity environment variables

--- src/utils/sanity-client.ts
+++ src/utils/sanity-client.ts
@@ -9,12 +9,14 @@ const isDeployPreview = process.env.CONTEXT === 'deploy-preview';
 const previewDrafts = STACKBIT_PREVIEW?.toLowerCase() === 'true' || SANITY_PREVIEW_DRAFTS?.toLowerCase() === 'true';
 
 export const sanityConfig: ClientConfig = {
-    projectId: SANITY_PROJECT_ID,
+    projectId: SANITY_PROJECT_ID || 'placeholder',
     dataset: SANITY_DATASET || 'production',
     useCdn: false,
     apiVersion: '2024-01-31',
-    token: SANITY_TOKEN,
+    token: SANITY_TOKEN || 'placeholder',
     perspective: isDev || isDeployPreview || previewDrafts ? 'previewDrafts' : 'published'
 };
 
-export const client = createClient(sanityConfig);
+export const client = SANITY_PROJECT_ID && SANITY_TOKEN && SANITY_PROJECT_ID !== 'placeholder' && SANITY_TOKEN !== 'placeholder' 
+    ? createClient(sanityConfig) 
+    : null;
